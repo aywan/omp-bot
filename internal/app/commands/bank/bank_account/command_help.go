7581f7efdc8cb14fabfd3ea2aaeeaa3c6f20ab7c
aywan <aywan@aywan.ru>
2021-10-18 23:32:25 +0700

    bank/bank_account help command

--- internal/app/commands/bank/bank_account/command_help.go
+++ internal/app/commands/bank/bank_account/command_help.go
@@ -7,8 +7,14 @@ import (
 
 func (c *BankBankAccountCommander) Help(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help\n"+
-			"/list - list products",
+		`Bank / Bank account commands:
+/help__bank__bank_account — print list of commands
+/get__bank__bank_account — get a entity
+/list__bank__bank_account — get a list of your entity
+/delete__bank__bank_account — delete an existing entity
+/new__bank__bank_account — create a new entity
+/edit__bank__bank_account — edit a entity
+`,
 	)
 
 	_, err := c.bot.Send(msg)
